89379681949a3df9217e02a11596b22d52f11976
Halgand-Valentin <valentin78700@gmail.com>
2026-04-21 16:35:56 +0200

    Resto.java
    Ajouts des différents choix (à compléter) et de l'option 4 pour quitter le programmes.

--- Resto.java
+++ Resto.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
-public class Resto{
-
+public class Restau{
 
     public static void proposerChoix(){
 
@@ -10,27 +9,32 @@ public class Resto{
         System.out.println("3. Utiliser la casse de secours");
         System.out.println("4. Quitter");
         System.out.println("------------------");
-        System.out.println("Choisi entre 1 et 4 : ");
+        System.out.println("Choisir entre 1 et 4 : ");
 
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         while (true) {
 
-            proposerChoix();
+            proposerChoix(); //Affiche les différents choix possible et recupere l entree utilisateur
             int choix = sc.nextInt();
 
-
-            while(choix < 1 || choix > 4){
-
+            while(choix < 1 || choix > 4){ // Verification de l entree utilisateur
                 System.out.println("Merci de choisir un chiffre entre 1 et 4");
                 proposerChoix();
                 choix= sc.nextInt();
-
             }
 
+            if(choix == 1) { //Afficher les détails d’un produit
 
+            } else if (choix == 2) { //Réaliser le paiement d’une table
+
+            } else if (choix == 3) { //Utiliser la caisse de secours
+
+            } else { //Quitter le programme
+                System.out.println("Au revoir !");
+                break;
+            }
         }
     }
-
 }
